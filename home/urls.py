c21056cc44423b63352ee1e5768e9015842ae31d
Lewis Cummings <cummingsjr00@gmail.com>
2024-09-11 10:33:06 +0000

    Added placeholder for products section

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', include('home.urls')),
+    path('products/', placeholder_view, name='products'),
 ]
