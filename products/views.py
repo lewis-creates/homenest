62a2ba5fc63c8b1a6413a6fc0a213499e31c454e
Lewis Cummings <cummingsjr00@gmail.com>
2024-09-17 13:48:00 +0000

    Fixed typo in views file that was causing issues

--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from .models import Product
 def all_products(request):
     """  A view to show all products, including sorting and search queries """
 
-    products = Products.objects.all()
+    products = Product.objects.all()
 
     context = {
         'products': products,
