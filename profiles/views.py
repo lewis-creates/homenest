8083b26ec7b6218c15e13eeabe24ff6ad05bbda5
Lewis Cummings <cummingsjr00@gmail.com>
2024-09-27 12:46:03 +0000

    Import user profile form

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,9 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
+from .forms import UserProfileForm
+
+from checkout.models import Order
 
 
 from checkout.models import Order
