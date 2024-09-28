62ddea77c5421b20f4d5df9ce31a8f648f94ad48
Lewis Cummings <cummingsjr00@gmail.com>
2024-09-28 15:54:18 +0000

    Deployment to heroku attempted

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn homenest.wsgi:application
--- homenest/settings.py
+++ homenest/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
+if os.path.exists("env.py"):
+    import env
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,9 +29,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-lewiscreates-homenest-ndyla0qohtx.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = [
+    '8000-lewiscreates-homenest-ndyla0qohtx.ws.codeinstitute-ide.net',
+    'localhost'
+    'homenest.herokuapp.com'
+    ]
 
 
 # Application definition
@@ -122,12 +130,17 @@ WSGI_APPLICATION = 'homenest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
